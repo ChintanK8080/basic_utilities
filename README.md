7b9f091068e8fc98799108019117e9fabc1f7124
chintan <100577004+ChintanK8080@users.noreply.github.com>
2023-09-04 18:18:22 +0530

    some basic methods added

--- README.md
+++ README.md
@@ -1,39 +1,13 @@
-<!--
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
+My Testing Package This is a private and deletable package that I created for testing purposes. It does not do anything useful, but it can be used to test the publishing and installation of private packages on pub.dev.
 
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
+Installation To install this package, you will need to have the Flutter SDK installed. Once you have the Flutter SDK installed, you can install the package by running the following command:
 
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages).
--->
+pub add my_testing_package
 
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
+Usage This package does not have any public APIs, so it cannot be used in your Flutter projects. However, you can use it to test the publishing and installation of private packages on pub.dev.
 
-## Features
+Contributing This package is not currently accepting contributions.
 
-TODO: List what your package can do. Maybe include images, gifs, or videos.
+License This package is licensed under the MIT License.
 
-## Getting started
-
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
-
-## Usage
-
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder.
-
-```dart
-const like = 'sample';
-```
-
-## Additional information
-
-TODO: Tell users more about the package: where to find more information, how to
-contribute to the package, how to file issues, what response they can expect
-from the package authors, and more.
+You can customize this README.md file to fit your specific needs. For example, you can add more information about the features of your package, or you can add instructions on how to use the package in your Flutter projects.
--- lib/basic_utilities_ck.dart
+++ lib/basic_utilities_ck.dart
@@ -1,5 +1,162 @@
 library basic_utilities_ck;
 
-class Utility {
-  int addOne(int value) => value + 1;
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class JsonEncodable {
+  Map<String, dynamic> toJson();
+}
+
+class BasicUtilitiesCk {
+  static Future<void> clearPref() async {
+    final pref = await SharedPreferences.getInstance();
+    await pref.clear();
+  }
+
+  static Future<void> storeUserModel<T extends JsonEncodable>(T? user) async {
+    final pref = await SharedPreferences.getInstance();
+    await pref.setString(
+      "UserPref",
+      jsonEncode(
+        user?.toJson(),
+      ),
+    );
+  }
+
+  static String? extensionRemover(String path) {
+    try {
+      final splited = path.split("?").first;
+      return splited.split('.').toList().last;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  static Future<void> setStringList(
+      {required String key, List<String>? data}) async {
+    if (data?.isNotEmpty ?? false) {
+      final pref = await SharedPreferences.getInstance();
+      await pref.setStringList(
+        key,
+        data!,
+      );
+    }
+  }
+
+  static Future<void> setString({required String key, String? data}) async {
+    if (data != null) {
+      final pref = await SharedPreferences.getInstance();
+      await pref.setString(key, data);
+    }
+  }
+
+  static Future<void> setDouble({required String key, double? data}) async {
+    if (data != null) {
+      final pref = await SharedPreferences.getInstance();
+      await pref.setDouble(key, data);
+    }
+  }
+
+  static Future<void> setBool({required String key, bool? data}) async {
+    if (data != null) {
+      final pref = await SharedPreferences.getInstance();
+      await pref.setBool(key, data);
+    }
+  }
+
+  static Future<bool?> getBool({required String key}) async {
+    final pref = await SharedPreferences.getInstance();
+    return pref.getBool(key);
+  }
+
+  static Future<void> setInt({required String key, int? data}) async {
+    if (data != null) {
+      final pref = await SharedPreferences.getInstance();
+      await pref.setInt(key, data);
+    }
+  }
+
+  static Future<int?> getInt({required String key}) async {
+    final pref = await SharedPreferences.getInstance();
+    return pref.getInt(key);
+  }
+
+  static Future<void> storeAccessToken(String? accessToken) async {
+    if (accessToken?.isNotEmpty ?? false) {
+      final pref = await SharedPreferences.getInstance();
+      await pref.setString("accessToken", accessToken!);
+    }
+  }
+
+  static Future<String?> getAccessToken() async {
+    final pref = await SharedPreferences.getInstance();
+    var data = pref.getString(
+      "accessToken",
+    );
+    return data;
+  }
+
+  static Future<void> storeAuthModel<T extends JsonEncodable>(T? user) async {
+    final pref = await SharedPreferences.getInstance();
+    await pref.setString(
+      "AuthModel",
+      jsonEncode(
+        user?.toJson(),
+      ),
+    );
+  }
+
+  static bool isValidEmail(String email) {
+    const pattern =
+        r'^[\w-]+(\.[\w-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,})$';
+    final regExp = RegExp(pattern);
+    return regExp.hasMatch(email);
+  }
+
+  static showSnackBar(BuildContext context,
+      {required String message, Color? color, Duration? duration}) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          message,
+          style: TextStyle(),
+          textAlign: TextAlign.center,
+        ),
+        backgroundColor: color,
+        padding: EdgeInsets.all(0),
+        duration: duration ?? Duration(seconds: 5),
+      ),
+    );
+  }
+
+  static showToast({
+    required msg,
+    Color? backgroundColor = Colors.white,
+    Color? textColor,
+  }) {
+    Fluttertoast.showToast(
+      msg: msg,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: backgroundColor,
+      textColor: textColor,
+      fontSize: 16.0,
+    );
+  }
+
+  static void logWithLocation(String message) {
+    String stackTrace = StackTrace.current.toString();
+    List<String> lines = stackTrace.split("\n");
+
+    String callerLine = lines[1].trim();
+    int startIndex = callerLine.indexOf(" ");
+    int endIndex = callerLine.lastIndexOf("(");
+
+    String callerFile = callerLine.substring(startIndex, endIndex).trim();
+    int? callerLineNumber =
+        int.tryParse(callerLine.substring(endIndex + 1, callerLine.length - 1));
+  }
 }
